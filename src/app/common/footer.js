cdd7a64cc908e1684720cf6e99d821c76e1163e9
somnath <somnathjana50@gmail.com>
2024-07-30 21:13:41 +0530

    laetst added

--- src/app/common/footer.js
+++ src/app/common/footer.js
@@ -5,6 +5,11 @@ export default function Footer() {
                 <div className="footerL">
                     <img src="./img/logo.png" className="logo" alt="" />
                 </div>
+                <div className="social-group">
+                   <a href="https://www.facebook.com/profile.php?id=100091499252113" target="_blank"><i className="fab fa-facebook-square share social-icon"></i></a>
+                   <a href="https://www.instagram.com/sunderban_weald_camp" target="_blank"><i className="fab fa-instagram-square share social-icon"></i></a>
+                    <a href="https://www.youtube.com/@sunderbanwealdcamp" target="_blank"><i className="fab fa-youtube-square share social-icon"></i></a>
+                </div>
                 <div className="footerM">
                     <h4>Location</h4>
                     <p>Bali 9, Bali Hatkhola, Gosaba, South 24 Pgs, West Bengal 743370, India.</p>
